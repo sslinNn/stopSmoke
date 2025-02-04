ef4171fef7162ffd63ec25bfdba2a758ac23241c
Belomestov Ivan <113080924+sslinNn@users.noreply.github.com>
2025-02-04 19:07:46 +0700

    /me route little fixes

--- BackEnd/utils/JWTUtils.py
+++ BackEnd/utils/JWTUtils.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timezone, timedelta
 from jose import jwt
+from sqlalchemy.util import await_only
+
 from backend.config import get_auth_data
 
 
@@ -12,3 +14,19 @@ async def create_access_token(data: dict) -> str:
         to_encode, auth_data["secret_key"], algorithm=auth_data["algorithm"]
     )
     return encode_jwt
+
+
+def get_payload_from_token(token: str) -> dict:
+    auth_data = get_auth_data()
+    payload = jwt.decode(
+        token=token,
+        key=auth_data["secret_key"],
+        algorithms="HS256",
+    )
+    return payload
+
+
+if __name__ == "__main__":
+    key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyNCIsImV4cCI6MTc0MTI2MjY4MX0.ynyHsRrKPqoPamHo4-6Yb7jZbqTuFHEzqoq1Hr2T90g"
+    data = get_payload_from_token(key)
+    print(data)
